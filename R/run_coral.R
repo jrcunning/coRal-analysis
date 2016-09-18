7d0e0d98d2282d8c09ca3dd70991acc654c351b9
Ross Cunning <ross.cunning@gmail.com>
2016-09-17 18:17:47 -1000

    update notation

--- R/run_coral.R
+++ R/run_coral.R
@@ -1,98 +1,97 @@
 run_coral <- function(time, env, pars) {
-  
   require(dplyr)
   
-  # Get initial values
+  # Get initial values 
   # Host initialization
-  rpars <- pars$rpars
-  R <- data_frame(
+  hpars <- pars$hpars
+  H <- data_frame(
     time=time,
-    R=rpars$initR,
-    UX=(rpars$UXm * env$X[1] / (env$X[1] + rpars$XKX)),
-    UCPa=rpars$UCPaf,
-    UCPp=rpars$UCPpf,
-    UCPt=UCPa+UCPp,
-    UN=(rpars$UNm * env$N[1] / (env$N[1] + rpars$XKN)),
-    rhoN=UN,
+    H=hpars$initH,
+    jX=(hpars$jXm * env$X[1] / (env$X[1] + hpars$KX)),
+    jCO2a=hpars$jCO2a,
+    jCO2p=hpars$jCO2p,
+    jCO2=jCO2a+jCO2p,
+    jN=(hpars$jNm * env$N[1] / (env$N[1] + hpars$KN)),
+    rhoN=jN,
     wC=0,
-    QR=0.25,
-    TR=rpars$gammaR,
-    rNR=TR * rpars$nNR * rpars$sigmaNR,
-    rCR=TR * rpars$sigmaCR
+    jHG=0.25,
+    HT=hpars$jHT0,
+    rNH=HT * hpars$nNH * hpars$sigmaNH,
+    rCH=HT * hpars$sigmaCH
   )
-  R[2:nrow(R), 2:ncol(R)] <- NA
+  H[2:nrow(H), 2:ncol(H)] <- NA
   
   # Symbiont initialization
   spars <- pars$spars
   S <- data_frame(
     time=time,
     S=spars$initS,
-    UL=env$L[1] * spars$ds,
-    UCS=max(0, synth(UL * spars$nLC, (R$UCPp[1] + R$UCPa[1])*R$R/S, spars$UCSm), na.rm=T),
-    eL=max(UL - UCS/spars$nLC, 0),
-    eC=R$UCPt*R$R/S - UCS,
-    QS=spars$QSm,
-    rhoC=UCS, 
+    jL=env$L[1] * spars$astar,
+    jCP=max(0, synth(jL * spars$nLC, (H$jCO2p[1] + H$jCO2a[1])*H$H/S, spars$jCPm), na.rm=T),
+    jeL=max(jL - jCP/spars$nLC, 0),
+    eC=H$jCO2*H$H/S - jCP,
+    jSG=spars$jSGm,
+    rhoC=jCP, 
     wN=0,
-    TS=spars$gammaS,
-    rNS=TS * spars$nNS * spars$sigmaNS,
-    rCS=TS * spars$sigmaCS,
-    dp=1)
+    ST=spars$jST0,
+    rNS=ST * spars$nNS * spars$sigmaNS,
+    rCS=ST * spars$sigmaCS,
+    cROS=1)
   S[2:nrow(S), 2:ncol(S)] <- NA
   
   # Run simulation by updating
   for (t in 2:length(time)) {
     # Symbiont biomass loss
-    # Make turnover rate increase twice the dp (ROS) flux? 5 times!!!!
-    S$TS[t] <- spars$gammaS + spars$gammaS * 5 * (S$dp[t-1] - 1) #+ 5 * (S$dp[t-1] - 1) # REMOVE TIMES 5 -- should be 5 * (dp - 1)
+    # Make turnover rate increase twice the cROS (ROS) flux? 5 times!!!!
+    S$ST[t] <- spars$jST0 * (1 + 5 * (S$cROS[t-1] - 1)) #+ 5 * (S$cROS[t-1] - 1) # REMOVE TIMES 5 -- should be 5 * (cROS - 1)
     # Coral biomass loss
-    R$TR[t] <- rpars$gammaR
+    H$HT[t] <- hpars$jHT0
     # Biomass recycling
-    S$rNS[t] <- S$TS[t] * spars$nNS * spars$sigmaNS
-    R$rNR[t] <- R$TR[t] * rpars$nNR * rpars$sigmaNR
-    S$rCS[t] <- S$TS[t] * spars$sigmaCS
-    R$rCR[t] <- R$TR[t] * rpars$sigmaCR
+    S$rNS[t] <- spars$jST0 * spars$nNS * spars$sigmaNS
+    H$rNH[t] <- H$HT[t] * hpars$nNH * hpars$sigmaNH
+    S$rCS[t] <- spars$jST0 * spars$sigmaCS
+    H$rCH[t] <- H$HT[t] * hpars$sigmaCH
     # Light uptake
-    #S$UL[t] <- env$L[t] * (1 + 2 * exp(-3.8 * (S$S[t-1]/R$R[t-1]))) * spars$ds   #- old
-    #S$UL[t] <- (0.6775 + 3.6947 * exp(-43.8762 * (S$S[t-1]/R$R[t-1]))) * env$L[t] * spars$ds  # aboral data only
-    #S$UL[t] <- (0.9977 + 2.4820 * exp(-39.0402 * (S$S[t-1]/R$R[t-1]))) * env$L[t] * spars$ds  # aboral and oral data
-    #S$UL[t] <- (0.9815 + (3 - 0.9815) * exp(-33.2613 * max(0, (S$S[t-1]/R$R[t-1])))) * env$L[t] * spars$ds  # aboral and oral with y-intercept=3
-    #S$UL[t] <- 1.667 * exp(-3.739 * (S$S[t-1]/R$R[t-1])) * env$L[t] * spars$ds  # nl2 -- no intercept
-    S$UL[t] <- (1.256307 + 1.385969 * exp(-6.479055 * (S$S[t-1]/R$R[t-1]))) * env$L[t] * spars$ds  # from Marcelino data
+    #S$jL[t] <- env$L[t] * (1 + 2 * exp(-3.8 * (S$S[t-1]/H$H[t-1]))) * spars$astar   #- old
+    #S$jL[t] <- (0.6775 + 3.6947 * exp(-43.8762 * (S$S[t-1]/H$H[t-1]))) * env$L[t] * spars$astar  # aboral data only
+    #S$jL[t] <- (0.9977 + 2.4820 * exp(-39.0402 * (S$S[t-1]/H$H[t-1]))) * env$L[t] * spars$astar  # aboral and oral data
+    #S$jL[t] <- (0.9815 + (3 - 0.9815) * exp(-33.2613 * max(0, (S$S[t-1]/H$H[t-1])))) * env$L[t] * spars$astar  # aboral and oral with y-intercept=3
+    #S$jL[t] <- 1.667 * exp(-3.739 * (S$S[t-1]/H$H[t-1])) * env$L[t] * spars$astar  # nl2 -- no intercept
+    S$jL[t] <- (1.256307 + 1.385969 * exp(-6.479055 * (S$S[t-1]/H$H[t-1]))) * env$L[t] * spars$astar  # from Marcelino data
     # DIC flux to photosynthesis SU
-    R$UCPp[t] <- rpars$UCPpf # passive CO2 flux due to reaction-diffusion
-    R$UCPa[t] <- rpars$UCPaf  # active delivery of CO2 by host to symbiont photosynthesis SU
-    R$UCPt[t] <- (R$UCPp[t] + R$UCPa[t])
+    H$jCO2p[t] <- hpars$jCO2p # passive CO2 flux due to reaction-diffusion
+    H$jCO2a[t] <- hpars$jCO2a  # active delivery of CO2 by host to symbiont photosynthesis SU
+    H$jCO2[t] <- (H$jCO2p[t] + H$jCO2a[t])
     # Photosynthetic production
-    S$UCS[t] <- synth(S$UL[t] * spars$nLC, R$UCPt[t]*R$R[t-1]/S$S[t-1] + S$rCS[t] + R$rCR[t]*R$R[t-1]/S$S[t-1], spars$UCSm) / S$dp[t-1]
+    S$jCP[t] <- synth(S$jL[t] * spars$nLC, H$jCO2[t]*H$H[t-1]/S$S[t-1] + S$rCS[t] + H$rCH[t]*H$H[t-1]/S$S[t-1], spars$jCPm) / S$cROS[t-1]
     # Symbiont biomass production
-    S$QS[t] <- synth(S$UCS[t], (R$rhoN[t-1]*R$R[t-1]/S$S[t-1] + S$rNS[t])/spars$nNS, spars$QSm)
+    S$jSG[t] <- synth(S$jCP[t], (H$rhoN[t-1]*H$H[t-1]/S$S[t-1] + S$rNS[t])/spars$nNS, spars$jSGm)
     # Heterotrophic food uptake
-    R$UX[t] <- (rpars$UXm * env$X[t] / (env$X[t] + rpars$XKX))
+    H$jX[t] <- (hpars$jXm * env$X[t] / (env$X[t] + hpars$KX))
     # DIN uptake
-    R$UN[t] <- (rpars$UNm * env$N[t] / (env$N[t] + rpars$XKN))
+    H$jN[t] <- (hpars$jNm * env$N[t] / (env$N[t] + hpars$KN))
     # Coral biomass production
-    R$QR[t] <- synth(S$rhoC[t-1]*S$S[t-1]/R$R[t-1] + R$UX[t], (R$UN[t] + rpars$nNX*R$UX[t] + R$rNR[t]) / rpars$nNR, rpars$QRm)
+    H$jHG[t] <- synth(S$rhoC[t-1]*S$S[t-1]/H$H[t-1] + H$jX[t], (H$jN[t] + hpars$nNX*H$jX[t] + H$rNH[t]) / hpars$nNH, hpars$jHGm)
     # Symbiont sharing of surplus carbon
-    S$rhoC[t] <- (S$UCS[t] - S$QS[t])
+    S$rhoC[t] <- (S$jCP[t] - S$jSG[t])
     # Coral sharing of surplus nitrogen
-    R$rhoN[t] <- R$UN[t] + rpars$nNX * R$UX[t] + R$rNR[t] - rpars$nNR * R$QR[t]
+    H$rhoN[t] <- H$jN[t] + hpars$nNX * H$jX[t] + H$rNH[t] - hpars$nNH * H$jHG[t]
     # Wasted nitrogen from symbiont
-    S$wN[t] <- R$rhoN[t]*R$R[t-1]/S$S[t-1] + S$rNS[t] - spars$nNS * S$QS[t]
+    S$wN[t] <- H$rhoN[t]*H$H[t-1]/S$S[t-1] + S$rNS[t] - spars$nNS * S$jSG[t]
     # Wasted carbon from coral
-    R$wC[t] <- R$UX[t] + S$rhoC[t]*S$S[t-1]/R$R[t-1] - R$QR[t]
+    H$wC[t] <- H$jX[t] + S$rhoC[t]*S$S[t-1]/H$H[t-1] - H$jHG[t]
     # Excess DIC from photosynthesis
-    S$eC[t] <- R$UCPt[t]*R$R[t-1]/S$S[t-1] + R$rCR[t]*R$R[t-1]/S$S[t-1] + S$rCS[t] - S$UCS[t]
+    S$eC[t] <- H$jCO2[t]*H$H[t-1]/S$S[t-1] + H$rCH[t]*H$H[t-1]/S$S[t-1] + S$rCS[t] - S$jCP[t]
     # Excess light energy from photosynthesis
-    S$eL[t] <- S$UL[t] - S$UCS[t]/spars$nLC
+    S$jeL[t] <- S$jL[t] - S$jCP[t]/spars$nLC
     # Damage potential (ROS) due to excess light energy
-    S$dp[t] <- 1 + (pmax(0, (S$eL[t] - spars$NPQ)) / spars$L50) ^ spars$k
+    S$cROS[t] <- 1 + (pmax(0, (S$jeL[t] - spars$jNPQ)) / spars$kROS) ^ spars$k
     # Balance equations: change in biomass
-    S$S[t] <- S$S[t-1] + (time[2] - time[1]) * (S$QS[t] - S$TS[t]) * S$S[t-1]
-    R$R[t] <- R$R[t-1] + (time[2] - time[1]) * (R$QR[t] - R$TR[t]) * R$R[t-1]
+    S$S[t] <- S$S[t-1] + (time[2] - time[1]) * (S$jSG[t] - S$ST[t]) * S$S[t-1]
+    H$H[t] <- H$H[t-1] + (time[2] - time[1]) * (H$jHG[t] - H$HT[t]) * H$H[t-1]
   }
   
   # Return results
-  return(list(time=time, env=env, rpars=rpars, spars=spars, R=R, S=S))
+  return(list(time=time, env=env, hpars=hpars, spars=spars, H=H, S=S))
 }
 
--- R/sym_PI.R
+++ R/sym_PI.R
@@ -1,21 +1,21 @@
 sym_PI <- function(pars) {
   with(pars, {
-    UL <- seq(0,200,1)
+    jL <- seq(0,200,1)
     # Set carbon delivery rate (mol C / CmolS / d)
-    UCP <- 100  # Extremely high carbon delivery - no carbon limitation
+    jCO2 <- 100  # Extremely high carbon delivery - no carbon limitation
     # Calculate photosynthesis rate
-    UCS <- synth(UCP, UL*spars$nLC, spars$UCSm)
+    jCP <- synth(jCO2, jL*spars$nLC, spars$jCPm)
     # Calculate light in excess of photosynthetic quenching
-    eL <- pmax(0, UL - UCS/spars$nLC)
-    # Calculate ROS (dp) produced due to excess light
-    dp <- pmax(1, 1 + (pmax(0, eL-spars$NPQ)/(spars$L50))^spars$k)
+    jeL <- pmax(0, jL - jCP/spars$nLC)
+    # Calculate ROS (cROS) produced due to excess light
+    cROS <- pmax(1, 1 + (pmax(0, jeL-spars$jNPQ)/(spars$kROS))^spars$k)
     # Calculate photosynthesis rate accounting for ROS-induced photoinhibition
-    UCS <- UCS / dp
+    jCP <- jCP / cROS
     # Return ROS and photosynthesis rate for plotting
     par(mfrow=c(1,2), mar=c(2,2,1,1), mgp=c(1.2,0.2,0), cex=0.7, tck=0.025, xaxs="i")
-    plot(UL, UCS, xlab="Light (mol photons/C-molS/d)", ylab="Carbon fixation (mol C/C-molS/d)",
+    plot(jL, jCP, xlab="Light (mol photons/C-molS/d)", ylab="Carbon fixation (mol C/C-molS/d)",
          main="Photosynthesis", type="l", lwd=3, col="red", ylim=c(0,3))
-    plot(UL, dp, xlab="Light (mol photons/C-molS/d)", ylab="ROS (relative to baseline)",
+    plot(jL, cROS, xlab="Light (mol photons/C-molS/d)", ylab="ROS (relative to baseline)",
          main="ROS production", type="l", lwd=3, col="orange", ylim=c(1,5))
   })
 }
