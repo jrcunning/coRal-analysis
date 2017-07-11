44bd216375ed046a36b483b4fcef2753e01ae267
Ross Cunning <ross.cunning@gmail.com>
2017-07-11 15:33:26 -0400

    tweak legend

--- R/Fig2.R
+++ R/Fig2.R
@@ -34,10 +34,10 @@ png("img/Fig2.png", width=5, height=5, units="in", res=300)
 plot(NA, xlim=c(20,50), ylim=c(0,0.20), xlab=expression("Light"~(mol~photons~m^{-2}~d^{-1})), 
      ylab=expression("Steady state S:H"~(C-mol~S~C-mol~H^{-1})))
 points(c(43,43,48,48), c(0.17,0.18,0.17,0.18), pch=c(19,1,19,1), cex=c(0.4,1,0.4,1), col=c("black","black","red","red"), xpd=T)
-text(41, 0.20, labels="init. S:H", xpd=T, cex=0.7, pos=2)
+text(35, 0.175, labels="init. S:H", xpd=T, cex=0.7, srt=90)
 text(41, c(0.17,0.18), labels=c("1", "0.0001"), xpd=T, pos=2, cex=0.7)
-text(46, 0.20, labels=c("Steady state growth"), xpd=T, cex=0.7)
-text(c(43,48), 0.19, labels=c(">0", "<0"), xpd=T, cex=0.7)
+text(45, 0.20, labels=c("Steady state growth"), xpd=T, cex=0.7)
+text(c(43,48), 0.19, labels=c("pos.", "neg."), xpd=T, cex=0.7)
 
 apply(res, 1, FUN=function(x) {
   with(x, points(L, sh,
